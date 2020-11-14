59bf8b886c55ae9f136244fed71ea1ddf6eea12f
Robert Payne <rjp5473@yahoo.com>
2020-11-14 09:48:30 -0500

    Commit final files

--- docs/app.js
+++ docs/app.js
@@ -1,10 +1,10 @@
-//Initialize Page
+//  Initialize Page
 function buildData(sample) {
-	//Use D3 fetch to read the JSON file
+	//  Use D3 fetch to read the JSON file
 	d3.json("samples.json").then((importedData) => {
 		var metadata = importedData.metadata;
-		var resultsarray = metadata.filter(sampleobject => sampleobject.id == sample);
-		var result = resultsarray[0]
+		var metaarray = metadata.filter(sampleobject => sampleobject.id == sample);
+		var result = metaarray[0]
 		var PANEL = d3.select("#sample-metadata");
 		PANEL.html("");
 		Object.entries(result).forEach(([key, value]) => {
@@ -12,27 +12,31 @@ function buildData(sample) {
 		});
 	});
 }
-//Begin Chart set-up 
+//  Begin Function and Chart set-up 
 
 function buildCharts(sample) {
 	d3.json("samples.json").then((importedData) => {
 		var samples = importedData.samples;
-		var resultsarray = samples.filter(sampleobject => sampleobject.id == sample);
-		var result = resultsarray[0]
-		//Grab Values from the response json object to build the graphs
+		var metaarray = samples.filter(sampleobject => sampleobject.id == sample);
+		var result = metaarray[0]
+		console.log(result);
+		//  Grab Values from the response json object to build the graphs
 		var ids = result.otu_ids;
 		var labels = result.otu_labels;
 		var values = result.sample_values;
-		//Build Bar Chart Top 10 OTU
-
-		var barchart = [{
+		console.log(values);
+		//  Build Bar Chart Top 10 OTU
+		//  Create Trace and reverse order for chart
+		var trace1 = {
 			y: ids.slice(0, 10).map(otuID => `OTU ${otuID}`).reverse(),
 			x: values.slice(0, 10).reverse(),
 			text: labels.slice(0, 10).reverse(),
 			type: "bar",
 			orientation: "h"
-		}];
-
+		};
+		//  Array for barchart
+		var barchart = [trace1];
+		//  Bar Chart Layout
 		var barchartLayout = {
 			title: "Top 10 OTUs",
 			margin: {
@@ -40,11 +44,11 @@ function buildCharts(sample) {
 				l: 150
 			}
 		};
-
+		//  Plot Barchart
 		Plotly.newPlot("bar", barchart, barchartLayout);
 
-		//Build Bubble Chart
-		var bubblechart = [{
+		//  Trace for bubblechart
+		var trace2 = {
 			x: ids,
 			y: values,
 			text: labels,
@@ -53,8 +57,10 @@ function buildCharts(sample) {
 				color: ids,
 				size: values,
 			}
-		}];
-
+		};
+		//  Array for bubblechart
+		var bubblechart = [trace2];
+		//  Bubblechart Layout
 		var bubbleLayout = {
 			margin: {
 				t: 0
@@ -64,14 +70,16 @@ function buildCharts(sample) {
 			},
 			hovermode: "closest",
 		};
-
+		//  Plot Bubblechart
 		Plotly.plot("bubble", bubblechart, bubbleLayout);
 	});
 }
-
+//  function when dropdown is selected
 function init() {
 	var selection = d3.select("#selDataset");
+	//  Use D3 to select dropdown
 	d3.json("samples.json").then((importedData) => {
+		//  Assign the value of dropdown menu to a variable
 		var sampleNames = importedData.names;
 		sampleNames.forEach((sample) => {
 			selection
